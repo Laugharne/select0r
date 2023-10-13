29a94b7e49d20a3fee17e3c6e013fbf0117b1784
laugharne <franck@maussand.net>
2023-10-13 15:41:47 +0200

    Add rust-crypto & test some results

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+extern crate num_cpus;
+extern crate crypto;
+
 use std::env;
 use std::process;
 use std::f64;
+use crypto::digest::Digest;
 use text_colorizer::*;
 
 
+type  IteratedValue           = u64;
+const BASE_NN: IteratedValue  = 64;
+const BASE_MAX: IteratedValue = BASE_NN-1;
+const BASE_BITS: u32          = BASE_MAX.count_ones();
+
 const SEPARATOR: u8 = '_' as u8;
 
-type  IterateValue           = u32; // u64 !?
-const BASE_NN: IterateValue  = 64;
-const BASE_MAX: IterateValue = BASE_NN-1;
-const BASE_BITS: u32         = BASE_MAX.count_ones();
-
-//const DIGIT_MAX: u32 = 2;	// 2 = test / 5 = release
 
 
 //	#[derive(Debug)]
@@ -66,14 +69,14 @@ fn init_app() -> Globals {
 	let part_n: &str = &args[0][..parenthesis];
 	let part_a: &str = &args[0][parenthesis..];
 
-	let _digit: u32 = f64::log(IterateValue::MAX as f64, BASE_NN as f64) as u32;
+	let _digit: u32 = f64::log(IteratedValue::MAX as f64, BASE_NN as f64) as u32;
 
 	Globals {
 		signature   : args[0].clone(),
 		part_name   : part_n.to_owned(),
 		part_args   : part_a.to_owned(),
 		difficulty  : args[1].parse::<u32>().unwrap(),
-		nn_threads  : 0,
+		nn_threads  : num_cpus::get() as u32,
 		digit_max   : 2,//digit+1,
 		leading_zero: match &*args[2] {"true"=>true, "false"=>false, _=>panic!("invalid leading zero value")},
 	}
@@ -86,14 +89,14 @@ fn compute() {
 }
 
 
-fn base64_to_suffix( digit: u32, value: IterateValue) -> Result<String, std::string::FromUtf8Error> {
+fn base64_to_suffix( digit: u32, value: IteratedValue) -> Result<String, std::string::FromUtf8Error> {
 	const ALPHABET: &[u8; BASE_NN as usize] = b"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$";
 
-	let mut value: IterateValue       = value;
-	let mut str_u8: [u8; 12] = [0; 12];
+	let mut value: IteratedValue = value;
+	let mut str_u8: [u8; 12]     = [0; 12];
+	let mut da: usize            = str_u8.len()-1;
 
-	let mut da: usize = str_u8.len()-1;
-	(0..digit).for_each(|_| {
+	(0..digit).for_each( |_| {
 		str_u8[da]   = ALPHABET[(value & BASE_MAX) as usize];
 		value      >>= BASE_BITS;
 		da -= 1;
@@ -112,9 +115,11 @@ fn base64_to_suffix( digit: u32, value: IterateValue) -> Result<String, std::str
 
 fn main_process(g: &Globals) {
 
-	(1..=g.digit_max).for_each(|digit| {
-		let max: IterateValue = 1 << (BASE_BITS*digit);
+	(1..=g.digit_max).for_each( |digit| {
+		let max: IteratedValue = 1 << (BASE_BITS*digit);
 		//println!("{} : {}", digit, max);
+
+		//(0..max).step_by(g.nn_threads).for_each( |value| {
 		for value in 0..max {
 
 			match base64_to_suffix(digit, value) {
@@ -125,8 +130,10 @@ fn main_process(g: &Globals) {
 					println!("Erreur de conversion : {:?}", e);
 				}
 			}
-			if value > 10 {break;}
 
+			if value > 18 {break;}
+
+		//});
 		}
 	});
 }
@@ -137,27 +144,18 @@ fn main() {
 
 	//println!("{:?}", g);
 	//println!("** {}", num_cpus::get());
+	//
+	let mut hasher = crypto::sha3::Sha3::keccak256();
+	let signature = "deposit278591A(uint)";
+	hasher.input_str(&signature);
+	let hash_result = hasher.result_str();
+	println!("{}\t{}", &hash_result[..8], signature);
+	//
 	main_process( &g);
 
 	process::exit(0);
 
 }
 
-/*
-
-
-use std::f64;
-
-fn main() {
-	let valeur: f64 = 64.0; // Exemple de valeur numérique
-
-	// Calcul du logarithme en base 64
-	let log_base_64 = f64::log(valeur, 64.0);
-
-	println!("Le logarithme en base 64 de {} est : {}",
-		valeur, (log_base_64 as u32)+1);
-}
-
-*/
 
 //	time cargo run "aaaa(uint)" 2 true
